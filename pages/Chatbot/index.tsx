59d8885e39a9b9afb755eea1685da5b565aac475
sandeep <mehta2155@outlook.com>
2021-12-18 03:44:14 +0530

    Added chat bot

---
+++ pages/Chatbot/index.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import ChatBot from "react-simple-chatbot";
+import { ThemeProvider } from "styled-components";
+
+const BotRedirect = ({ url, message }) => {
+  return (
+    <div>
+      <a href={url} target="_blank">
+        {message}
+      </a>
+    </div>
+  );
+};
+
+const CHATBOT_THEME = {
+  background: "#FFFEFC",
+  fontFamily: "Roboto",
+  headerBgColor: "#eec75c",
+  headerFontColor: "#fff",
+  headerFontSize: "15px",
+  botBubbleColor: "#C8D7C2",
+  botFontColor: "#fff",
+  userBubbleColor: "#eec75c",
+  userFontColor: "#fff"
+};
+
+const ChatBotHelper = () => {
+  const steps = [
+     {
+      id: "1",
+      message: "Hi, What is your name?",
+      trigger: "name"
+    },
+    {
+      id: "name",
+      user: true,
+      trigger: "3"
+    },
+    {
+      id: "3",
+      message: "Hi {previousValue}! please select your gender?",
+      trigger: "4"
+    },
+    {
+      id: "4",
+      options: [
+        { value: "male", label: "Male", trigger: "5" },
+        { value: "female", label: "Female", trigger: "5" }
+      ]
+    },
+    {
+      id: "5",
+      message: "How old are you?",
+      user: true,
+      validator: (value: number) => {
+        if (isNaN(value)) {
+          return "value must be a number";
+        } else if (value < 0) {
+          return "value must be positive";
+        } else if (value > 120) {
+          return `${value}? Come on!`;
+        }
+
+        return true;
+      },
+    //   trigger: "age"
+    end: true
+    },
+  ];
+
+  return (
+    <>
+      <ThemeProvider theme={CHATBOT_THEME}>
+        <ChatBot steps={steps}  speechSynthesis={{ enable: true, lang: "en" }} floating={true} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default ChatBotHelper;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faAt, faHome, faPhoneAlt, faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
 import Home from "./HomeContainer.tsx"
 import React, { Component } from "react";
+import Chatbot from "./Chatbot";
 
 library.add(
 faPhoneVolume,
@@ -12,7 +13,10 @@ faPhoneAlt
 );
 
 const MyApp = ({ Component, pageProps}) => (
+  <>
   <Component {...pageProps} />
+  <Chatbot />
+  </>
 )
 
 export default MyApp;
